15c4b3b89438cbb0bd5c8d6bda87e87f0733413d
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2025-01-13 22:54:39 +0300

    finish half home screen

--- seller/src/main/java/com/app/partyzone/seller/ui/screen/home/HomeScreen.kt
+++ seller/src/main/java/com/app/partyzone/seller/ui/screen/home/HomeScreen.kt
@@ -1,43 +1,37 @@
 package com.app.partyzone.seller.ui.screen.home
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.app.partyzone.design_system.composable.PzNavigationBar
-import com.app.partyzone.design_system.composable.PzNavigationBarItem
+import com.app.partyzone.design_system.composable.PzChip
+import com.app.partyzone.design_system.composable.PzIconButton
 import com.app.partyzone.design_system.theme.Theme
-import com.app.partyzone.design_system.theme.gradientColors
+import com.app.partyzone.design_system.theme.brush
+import com.app.partyzone.seller.R
 
 @Composable
 fun HomeScreen() {
@@ -58,52 +52,90 @@ private fun HomeContent() {
             .background(Theme.colors.primary)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom
     ) {
-
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.welcome),
+                    color = Theme.colors.contentPrimary,
+                    style = Theme.typography.titleLarge
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = com.app.partyzone.design_system.R.drawable.user_icon),
+                        contentDescription = stringResource(R.string.map_icon),
+                        modifier = Modifier
+                            .size(24.dp)
+                            .graphicsLayer(alpha = 0.99f)
+                            .drawWithCache {
+                                onDrawWithContent {
+                                    drawContent()
+                                    drawRect(brush, blendMode = BlendMode.SrcAtop)
+                                }
+                            }
+                    )
+                    Text(
+                        text = "Ahmedabad, Gujarat", //user name
+                        color = Theme.colors.contentSecondary,
+                        style = Theme.typography.title
+                    )
+                }
+            }
+            PzIconButton(
+                painter = painterResource(id = com.app.partyzone.design_system.R.drawable.notification_icon),
+                onClick = { }) {
+            }
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(horizontal = 16.dp)
+                .border(1.dp, Theme.colors.contentBorder, RoundedCornerShape(Theme.radius.large))
+                .clip(RoundedCornerShape(Theme.radius.large))
+                .background(Theme.colors.primary, RoundedCornerShape(Theme.radius.large))
+                .clickable { },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row {
+                Spacer(modifier = Modifier.width(16.dp))
+                Icon(
+                    painter = painterResource(id = com.app.partyzone.design_system.R.drawable.search_icon),
+                    contentDescription = stringResource(R.string.search_icon),
+                    tint = Theme.colors.contentTertiary,
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(R.string.search),
+                    color = Theme.colors.contentTertiary,
+                    style = Theme.typography.title
+                )
+            }
+            Icon(
+                painter = painterResource(id = com.app.partyzone.design_system.R.drawable.filter_icon),
+                contentDescription = stringResource(R.string.search_icon),
+                tint = Theme.colors.contentPrimary,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(24.dp)
+                    .clickable { }
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.width(16.dp))
+        PzChip(label = "Music", isSelected = false, onClick = {})
     }
 }
-
-//@Composable
-//private fun BottomNavigationBarPreview() {
-//    var selectedItem by remember { mutableStateOf(3) }
-//    val items = listOf("Overview", "Taxis", "Restaurants", "Users", "sds")
-//    var xIndicatorOffset by remember { mutableStateOf(Float.NaN) }
-//    val xOffsetAnimated by animateFloatAsState(targetValue = xIndicatorOffset, label = "")
-//    val indicatorWidthPx = 40.dp.toPx()
-//    val iconSizePx = 2.dp.toPx()
-//
-//    PzNavigationBar(
-//        modifier = Modifier
-//            .drawTopIndicator(xOffsetAnimated)
-//            .padding(WindowInsets.navigationBars.asPaddingValues())
-//    ) {
-//        items.forEachIndexed { index, item ->
-//            PzNavigationBarItem(
-//                icon = { tint ->
-//                    Icon(
-//                        painter = painterResource(com.app.partyzone.design_system.R.drawable.home_icon),
-//                        contentDescription = item,
-//                        modifier = Modifier
-//                            .size(24.dp)
-//                            .onGloballyPositioned {
-//                                if (selectedItem == index) {
-//                                    xIndicatorOffset =
-//                                        it.positionInRoot().x + (iconSizePx - indicatorWidthPx) / 2
-//                                }
-//                            }
-//                            .graphicsLayer(alpha = 0.99f)
-//                            .drawWithCache {
-//                                onDrawWithContent {
-//                                    drawContent()
-//                                    drawRect(tint, blendMode = BlendMode.SrcAtop)
-//                                }
-//                            },
-//                    )
-//                },
-//                selected = selectedItem == index,
-//                onClick = { selectedItem = index }
-//            )
-//        }
-//    }
-//}
