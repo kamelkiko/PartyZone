12b46d08a580d578b2c23f84d9b62cf2fedfc9d1
kamel <kikokeko10@gmail.com>
2025-01-24 22:56:41 +0200

    init update profile logic

--- core/src/main/java/com/app/partyzone/core/domain/entity/UpdateUser.kt
+++ core/src/main/java/com/app/partyzone/core/domain/entity/UpdateUser.kt
@@ -1,3 +1,9 @@
 package com.app.partyzone.core.domain.entity
 
-data class UpdateUser()
+data class UpdateUser(
+    val name: String,
+    val email: String,
+    val oldPassword: String,
+    val newPassword: String,
+    val photoUrl: String?
+)
